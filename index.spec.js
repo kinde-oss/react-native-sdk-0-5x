8c969dc1dbf5fa4b931b8ede3ce96c652c9606a1
Trung <trung@thebigredhouse.com.au>
2022-10-27 16:03:11 +0700

    update unit tests

---
+++ index.spec.js
@@ -0,0 +1,266 @@
+const { KindeSDK, ApiClient, UserApi } = require(process.cwd() + '/src/index');
+import { Linking } from 'react-native';
+import Url from 'url-parse';
+
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+        json: () =>
+            Promise.resolve({
+                access_token: 'this_is_access_token',
+                scope: 'this_is_scope',
+                state: 'this_is_state',
+                token_type: 'this_is_token_type'
+            })
+    })
+);
+
+const configuration = {
+    issuer: 'https://myhost.kinde.com',
+    redirectUri: 'myapp://myhost.kinde.com/kinde_callback',
+    clientId: 'spa@live',
+    logoutRedirectUri: 'myapp://myhost.kinde.com/kinde_callback',
+    scope: 'openid offline',
+    authorizationEndpoint: 'https://myhost.kinde.com/oauth2/auth',
+    tokenEndpoint: 'https://myhost.kinde.com/oauth2/token',
+    logoutEndpoint: 'https://myhost.kinde.com/logout',
+    fakeState: 'uUj8nEDL-jxeDbS_si86i7UsFmG5ewf0axDu96pdHGc',
+    fakeCodeVerifier: 'K9E0HqVA4oxGuJqFWoasgmGKzI3Uxehdr9nTF2jaLR8',
+    fakeCodeChallenge: '3Aqg8_tu8aNwnxPmhE1b1ONsThy-b6hppET0knva9Kc'
+};
+
+const fakeUserProfile = {
+    id: 'kp:58ece9f68a7c4c098efc1cf45c774e16',
+    last_name: 'test',
+    first_name: 'user',
+    provided_id: null,
+    preferred_email: 'usertesting@yopmail.com'
+};
+jest.mock('Linking', () => ({
+    openURL: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn()
+}));
+
+jest.mock('AsyncStorage', () => ({
+    getItem: jest.fn().mockReturnValue('random_value'),
+    setItem: jest.fn()
+}));
+
+jest.mock('./src/SDK/Utils', () => ({
+    generateChallenge: jest.fn().mockReturnValue({
+        state: 'uUj8nEDL-jxeDbS_si86i7UsFmG5ewf0axDu96pdHGc',
+        codeVerifier: 'K9E0HqVA4oxGuJqFWoasgmGKzI3Uxehdr9nTF2jaLR8',
+        codeChallenge: '3Aqg8_tu8aNwnxPmhE1b1ONsThy-b6hppET0knva9Kc'
+    }),
+    generateRandomString: jest
+        .fn()
+        .mockReturnValue('uUj8nEDL-jxeDbS_si86i7UsFmG5ewf0axDu96pdHGc'),
+    checkNotNull: jest.fn((reference, name) => {
+        if (reference === null || reference === undefined) {
+            throw new Error(`${name} cannot be empty`);
+        }
+        return reference;
+    })
+}));
+
+describe('KindeSDK', () => {
+    beforeEach(() => {
+        fetch.mockClear();
+    });
+    describe('Initial', () => {
+        test('throws an error when issuer is not passed', () => {
+            expect(() => {
+                new KindeSDK();
+            }).toThrow('Issuer cannot be empty');
+        });
+
+        test('throws an error when redirectUrl is not passed', () => {
+            expect(() => {
+                new KindeSDK(configuration.issuer);
+            }).toThrow('Redirect URI cannot be empty');
+        });
+
+        test('throws an error when Client ID is not passed', () => {
+            expect(() => {
+                new KindeSDK(configuration.issuer, configuration.redirectUri);
+            }).toThrow('Client Id cannot be empty');
+        });
+
+        test('throws an error when Client ID is not passed', () => {
+            expect(() => {
+                new KindeSDK(configuration.issuer, configuration.redirectUri);
+            }).toThrow('Client Id cannot be empty');
+        });
+
+        test('throws an error when logoutRedirectUri is not passed', () => {
+            expect(() => {
+                new KindeSDK(
+                    configuration.issuer,
+                    configuration.redirectUri,
+                    configuration.clientId
+                );
+            }).toThrow('Logout Redirect URI');
+        });
+
+        test('Matching authorizationEndpoint', () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            expect(client.authorizationEndpoint).toBe(
+                configuration.authorizationEndpoint
+            );
+        });
+
+        test('Matching tokenEndpoint', () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            expect(client.tokenEndpoint).toBe(configuration.tokenEndpoint);
+        });
+
+        test('Matching logoutEndpoint', () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            expect(client.logoutEndpoint).toBe(configuration.logoutEndpoint);
+        });
+    });
+    describe('Redirect', () => {
+        test('Open authenticate endpoint', async () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            await client.login();
+            const URLParsed = Url(`${configuration.issuer}/oauth2/auth`, true);
+            URLParsed.query['client_id'] = configuration.clientId;
+            URLParsed.query['redirect_uri'] = configuration.redirectUri;
+            URLParsed.query['client_secret'] = configuration.clientSecret;
+            URLParsed.query['grant_type'] = 'authorization_code';
+            URLParsed.query['scope'] = configuration.scope;
+            URLParsed.query['start_page'] = 'login';
+            URLParsed.query['response_type'] = 'code';
+            URLParsed.query['state'] = configuration.fakeState;
+            URLParsed.query['code_challenge'] = configuration.fakeCodeChallenge;
+            URLParsed.query['code_challenge_method'] = 'S256';
+            expect(Linking.openURL).toHaveBeenCalledWith(URLParsed.toString());
+        });
+        test('Open registration endpoint', async () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            await client.register();
+            const URLParsed = Url(configuration.authorizationEndpoint, true);
+            URLParsed.query['client_id'] = configuration.clientId;
+            URLParsed.query['redirect_uri'] = configuration.redirectUri;
+            URLParsed.query['client_secret'] = configuration.clientSecret;
+            URLParsed.query['grant_type'] = 'authorization_code';
+            URLParsed.query['scope'] = configuration.scope;
+            URLParsed.query['start_page'] = 'registration';
+            URLParsed.query['response_type'] = 'code';
+            URLParsed.query['state'] = configuration.fakeState;
+            URLParsed.query['code_challenge'] = configuration.fakeCodeChallenge;
+            URLParsed.query['code_challenge_method'] = 'S256';
+            expect(Linking.openURL).toHaveBeenCalledWith(URLParsed.toString());
+        });
+        test('Logout', async () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            await client.logout();
+            const URLParsed = Url(configuration.logoutEndpoint, true);
+            URLParsed.query['redirect'] = configuration.logoutRedirectUri;
+            expect(Linking.openURL).toHaveBeenCalledWith(URLParsed.toString());
+        });
+    });
+    describe('Token', () => {
+        test('throws an error when url is not passed', async () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            await expect(client.getToken()).rejects.toThrow(Error);
+        });
+        test('throws an error when missing code in query', async () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            await expect(
+                client.getToken(configuration.redirectUri)
+            ).rejects.toThrow(Error);
+        });
+        test('throws an error when have error from callback', async () => {
+            expect.assertions(1);
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            try {
+                await client.getToken(
+                    `${configuration.redirectUri}?code=random_code&error=invalid`
+                );
+            } catch (error) {
+                expect(error).toMatch('invalid');
+            }
+        });
+        test('Get Token instance', async () => {
+            const client = new KindeSDK(
+                configuration.issuer,
+                configuration.redirectUri,
+                configuration.clientId,
+                configuration.logoutRedirectUri
+            );
+            await client.login();
+            const token = await client.getToken(
+                `${configuration.redirectUri}?code=random_code`
+            );
+            expect(token).toEqual({
+                access_token: 'this_is_access_token',
+                scope: 'this_is_scope',
+                state: 'this_is_state',
+                token_type: 'this_is_token_type'
+            });
+            expect(fetch).toHaveBeenCalledTimes(1);
+        });
+    });
+    describe('User Profile', () => {
+        test('Get User Profile', async () => {
+            const apiClient = new ApiClient(configuration.issuer);
+            const apiInstance = new UserApi(apiClient);
+            jest.spyOn(apiClient, 'callApi').mockImplementation(() => {
+                return {
+                    id: 'kp:58ece9f68a7c4c098efc1cf45c774e16',
+                    last_name: 'test',
+                    first_name: 'user',
+                    provided_id: null,
+                    preferred_email: 'usertesting@yopmail.com'
+                };
+            });
+            await expect(apiInstance.getUserProfile()).toEqual(fakeUserProfile);
+        });
+    });
+});
--- src/SDK/KindeSDK.js
+++ src/SDK/KindeSDK.js
@@ -1,5 +1,5 @@
 import { checkNotNull } from './Utils';
-import Url from 'url-parse';
+import Url, { qs } from 'url-parse';
 import { Linking } from 'react-native';
 import Storage from './Storage';
 import AuthorizationCode from './OAuth/AuthorizationCode';
@@ -16,11 +16,11 @@ export default class KindeSDK extends Storage {
         this.issuer = issuer;
         checkNotNull(this.issuer, 'Issuer');
 
-        this.clientId = clientId;
-        checkNotNull(this.clientId, 'Client Id');
-
         this.redirectUri = redirectUri;
         checkNotNull(this.redirectUri, 'Redirect URI');
+
+        this.clientId = clientId;
+        checkNotNull(this.clientId, 'Client Id');
 
         this.logoutRedirectUri = logoutRedirectUri;
         checkNotNull(this.logoutRedirectUri, 'Logout Redirect URI');
@@ -29,53 +29,58 @@ export default class KindeSDK extends Storage {
     }
 
     async login() {
-        this.cleanUp();
+        await this.cleanUp();
         const auth = new AuthorizationCode();
         return auth.login(this, true);
     }
 
     getToken(url) {
-        checkNotNull(url, 'URL');
         return new Promise(async (resolve, reject) => {
-            const URLParsed = Url(url, true);
-            const { code, error, error_description } = URLParsed.query;
-            checkNotNull(code, 'code');
-            if (error) {
-                const msg = error_description ? error_description : error;
-                throw new Error(msg);
-            }
-            const formData = new FormData();
-            formData.append('code', code);
-            formData.append('client_id', this.clientId);
-            formData.append('client_secret', null);
-            formData.append('grant_type', 'authorization_code');
-            formData.append('redirect_uri', this.redirectUri);
-            const state = await this.getState();
-            if (state) {
-                formData.append('state', state);
-            }
-            const codeVerifier = await this.getCodeVerifier();
-            formData.append('code_verifier', codeVerifier);
-            checkNotNull(code, 'Code Verifier');
-            const that = this;
-            fetch(this.tokenEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                },
-                body: formData
-            })
-                .then((response) => response.json())
-                .then(async (responseJson) => {
-                    if (responseJson.error) {
-                        return reject(responseJson);
-                    }
-                    await that.setAccessToken(responseJson.access_token);
-                    resolve(responseJson);
+            try {
+                checkNotNull(url, 'URL');
+                const URLParsed = Url(url, true);
+                const { code, error, error_description } = URLParsed.query;
+                checkNotNull(code, 'code');
+                if (error) {
+                    const msg = error_description ? error_description : error;
+                    reject(msg);
+                }
+                const formData = {
+                    code,
+                    client_id: this.clientId,
+                    client_secret: null,
+                    grant_type: 'authorization_code',
+                    redirect_uri: this.redirectUri
+                };
+                const state = await this.getState();
+                if (state) {
+                    formData['state'] = state;
+                }
+                const codeVerifier = await this.getCodeVerifier();
+                formData['code_verifier'] = codeVerifier;
+                checkNotNull(code, 'Code Verifier');
+                const that = this;
+                fetch(this.tokenEndpoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                    body: qs.stringify(formData)
                 })
-                .catch((err) => {
-                    reject(err.response.data);
-                });
+                    .then((response) => response.json())
+                    .then(async (responseJson) => {
+                        if (responseJson.error) {
+                            return reject(responseJson);
+                        }
+                        await that.setAccessToken(responseJson.access_token);
+                        resolve(responseJson);
+                    })
+                    .catch((err) => {
+                        reject(err.response.data);
+                    });
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 
@@ -92,11 +97,11 @@ export default class KindeSDK extends Storage {
     }
 
     async cleanUp() {
-        await Promise.all(
-            this.setState(null),
-            this.setAccessToken(null),
-            this.setCodeVerifier(null)
-        );
+        return Promise.all([
+            this.setState(''),
+            this.setAccessToken(''),
+            this.setCodeVerifier('')
+        ]);
     }
 
     get authorizationEndpoint() {
