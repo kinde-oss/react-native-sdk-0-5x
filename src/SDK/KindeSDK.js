76b5b6dfc95936e7244af890a92a48061f6e9a12
Phuong To <phuongwd@gmail.com>
2023-04-12 23:25:01 +0700

    feat(auth): add new method refreshToken

--- src/SDK/KindeSDK.js
+++ src/SDK/KindeSDK.js
@@ -102,12 +102,7 @@ export default class KindeSDK {
                 return token;
             }
 
-            const formData = new FormData();
-            formData.append('client_id', this.clientId);
-            formData.append('client_secret', this.clientSecret);
-            formData.append('grant_type', 'refresh_token');
-            formData.append('refresh_token', token.refresh_token);
-            return this.fetchToken(formData);
+            return this.refreshToken(token.refresh_token);
         }
 
         if (this.checkIsUnAuthenticated()) {
@@ -161,6 +156,20 @@ export default class KindeSDK {
             this.updateAuthStatus(authStatusConstants.AUTHENTICATED);
             resolve(dataResponse);
         });
+    }
+
+    /**
+     * Sends a request to the authentication server to refresh the tokens using the provided refresh token.
+     * @param {String} refresh_token - The refresh token issued to the user.
+     * @return A promise.
+     */
+    refreshToken(refresh_token) {
+        const formData = new FormData();
+        formData.append('client_id', this.clientId);
+        formData.append('client_secret', this.clientSecret);
+        formData.append('grant_type', 'refresh_token');
+        formData.append('refresh_token', refresh_token);
+        return this.fetchToken(formData);
     }
 
     /**
